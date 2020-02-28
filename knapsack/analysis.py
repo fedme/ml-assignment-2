2d849f5acbf2fc43d45f3bc7a83ffbd5829e50e6
Federico Meini <fme@templafy.com>
2020-02-28 15:12:34 +0100

    Add problem size to plots titles

--- knapsack/analysis.py
+++ knapsack/analysis.py
@@ -9,12 +9,12 @@ def plot_iterations_vs_fitness(size=100):
     for algo in ['rhc', 'sa', 'ga', 'mimic']:
         df = pd.read_csv(f'{optimization.STATS_FOLDER}/{algo}_{size}_stats.csv')
         plt.plot(df['fitness'], label=algo)
-    plt.title(f'{optimization.PROBLEM_NAME.capitalize()} - Iterations vs Fitness')
+    plt.title(f'{optimization.PROBLEM_NAME.capitalize()} with size {size} - Iterations vs Fitness')
     plt.xlabel('Iterations')
     plt.ylabel('Fitness')
     plt.legend()
     plt.grid()
-    plt.savefig(f'{PLOTS_FOLDER}/{optimization.PROBLEM_NAME}_{size}_iterations_vs_fitness.png')
+    plt.savefig(f'{PLOTS_FOLDER}/{optimization.PROBLEM_NAME}_size{size}_iterations_vs_fitness.png')
     plt.clf()
 
     print(f'{optimization.PROBLEM_NAME} iterations vs fitness plotted.')
